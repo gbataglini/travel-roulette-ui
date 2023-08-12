040fc515369b54d57cd92b73acbbbe0d92551a36
Giovanna Bataglini <gibataglini@gmail.com>
2023-08-13 00:32:01 +0100

    Add Destination functionality

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,6 +1,33 @@
 import NavBar from "../components/NavBar";
+import { useState } from "react";
 
 export default function Home() {
+  const [destination, setDestination] = useState("");
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("http://localhost:5001/api/v1/destinations", {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          destinationName: destination,
+        }),
+      });
+
+      if (res.status === 200) {
+        setDestination("");
+        console.log("Destination added successfully");
+      } else {
+        alert("Error! Could not add destination. ");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <>
       <div
@@ -14,14 +41,22 @@ export default function Home() {
             </div>
 
             <div className="py-8 flex justify-center">
-              <p className="text-6xl font-bold text-black">Where to next?</p>
+              <p className="text-7xl font-bold italic text-black">WHERE TO?</p>
             </div>
 
             <div className="flex justify-center">
-              <form>
-                <input className="border-solid border-2 rounded-lg bg-transparent p-2" />
+              <form onSubmit={handleSubmit}>
+                <input
+                  className="border-solid border-2 rounded-lg bg-transparent p-2"
+                  type="text"
+                  value={destination}
+                  onChange={(e) => setDestination(e.target.value)}
+                />
 
-                <button className="bg-white hover:bg-black hover:text-white rounded-lg p-2 ml-3">
+                <button
+                  className="bg-white hover:bg-black hover:text-white rounded-lg p-2 ml-3"
+                  type="submit"
+                >
                   Add
                 </button>
               </form>
